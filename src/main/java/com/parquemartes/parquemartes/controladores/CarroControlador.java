14443fa0694134dc600ced8a56858bd93b208418
Neider <jhonacevedocardenas@iesantaelena.edu.co>
2021-09-15 08:29:19 -0500

    resto de clases

--- src/main/java/com/parquemartes/parquemartes/controladores/CarroControlador.java
+++ src/main/java/com/parquemartes/parquemartes/controladores/CarroControlador.java
@@ -5,10 +5,66 @@
  */
 package com.parquemartes.parquemartes.controladores;
 
+import com.parquemartes.parquemartes.entidades.Carro;
+import com.parquemartes.parquemartes.servicios.CarroServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author NEIDER
  */
+
+@RestController
+@CrossOrigin(origins="*")
+@RequestMapping("IS/v1/carros")
 public class CarroControlador {
+    @Autowired
+    private CarroServicio carroServicio;
     
+    @PostMapping("")
+    public ResponseEntity<?> RegistrarIngresoCarro(@RequestBody Carro carro){
+        
+        try{
+            
+             return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(carroServicio.ingresarCarro(carro));
+            
+        }catch(Exception error){
+            
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("{error:intenta nuevamente}");
+            
+        }
+        
+    }
+    
+    @PutMapping("/{placa}")
+    public ResponseEntity<?> RegistrarSalidaCarro(@PathVariable String placa){
+        
+        try{
+            
+             return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(carroServicio.registrarSalida(placa));
+            
+        }catch(Exception error){
+            
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("{error:intenta nuevamente}");
+            
+        }
+        
+    }
 }
--- src/main/java/com/parquemartes/parquemartes/controladores/ParqueaderoControlador.java
+++ src/main/java/com/parquemartes/parquemartes/controladores/ParqueaderoControlador.java
@@ -5,10 +5,66 @@
  */
 package com.parquemartes.parquemartes.controladores;
 
+import com.parquemartes.parquemartes.entidades.Parqueadero;
+import com.parquemartes.parquemartes.servicios.ParqueaderoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author NEIDER
  */
+@RestController
+@CrossOrigin(origins="*")
+@RequestMapping("IS/v1/parqueaderos")
 public class ParqueaderoControlador {
+    @Autowired
+    private ParqueaderoServicio parqueaderoServicio;
     
+    @GetMapping("/{id}")
+    public ResponseEntity<?> consultarInformacion(@PathVariable int id){
+        
+        try{
+            
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(parqueaderoServicio.obtenerInformacion(id));
+            
+        }catch(Exception error){
+            
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("{error:intenta nuevamente}");
+            
+        }
+        
+    }
+    
+    @PutMapping
+    public ResponseEntity<?> editarInformacion(@PathVariable int id, @RequestBody Parqueadero parqueadero){
+        
+        try{
+            
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(parqueaderoServicio.editarInformacion(id, parqueadero));
+            
+        }catch(Exception error){
+            
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("{error:intenta nuevamente}");
+            
+        }
+        
+        
+    }
 }
